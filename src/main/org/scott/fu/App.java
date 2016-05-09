255f8f90df81e391271b06467377ee5f91e25e77
scottfu1644 <scottfu1644@hotmail.com>
2016-05-09 09:01:35 +0800

    commit secondary

---
+++ src/main/org/scott/fu/App.java
@@ -0,0 +1,10 @@
+package org.scott.fu;
+
+/**
+ * Created by ScottFu on 2016/5/9.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello afdssd");
+    }
+}
